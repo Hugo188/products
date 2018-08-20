79895f277ffb4ac03d304101191dc54ffaf576df
Hugo188 <inotgay@hotmil.com>
2018-08-20 14:56:51 +0800

    add check file

--- products.py
+++ products.py
@@ -1,16 +1,18 @@
+import os #operating system
+
 #讀取檔案
 products = []
-with open('products.csv', 'r') as f:
-#with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		#s = line.strip().split(',')
-		#name = s[0]
-		#price = s[1]
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):#檢查檔案在不再
+	print('檔案已建構')
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('無檔案...')
 
 #讓使用者輸入
 while True:
@@ -18,17 +20,16 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
 #印出所有購買紀錄
 for p in products:
-	print(p)
 	print(p[0],'的價格是', p[1])
 
 #寫入檔案
 with open('products.csv', 'w') as f: 
-#with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
